a6cbbc0b2e4b876e863368399e43bb5b64ffb9a4
amlshohdy <amlshohdy667@gmail.com>
2021-04-27 07:53:46 +0200

    adding Aml

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index Hello Ahmad Branch.")
+    return HttpResponse("Hello, world. You're at the polls index Hello Ahmad Aml Branch.")
 
